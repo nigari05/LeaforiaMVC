8db534c61944c17b1891369ee2f9b4cfeac480bd
Nigari Zulfuqarova <nigarizulfuqarova@gmail.com>
2026-08-23 21:22:27 +0400

    feat(database): configure AppDbContext and app settings
    - Updated AppDbContext with new entity configurations
    - Added connection string to appsettings.json
    - Registered DbContext and dependencies in Program.cs
    - Updated project package dependencies (.csproj)

--- WebApplication4/Data/AppDbContext.cs
+++ WebApplication4/Data/AppDbContext.cs
@@ -1,6 +1,27 @@
-﻿namespace WebApplication4.Data
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ContactMessage> ContactMessages { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<NewsletterSubscriber> NewsletterSubscribers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+
+
+
     }
 }

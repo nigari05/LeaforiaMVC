5255c4bb63409b8b46efbf5ed09b096af55d508e
Nigari Zulfuqarova <nigarizulfuqarova@gmail.com>
2026-08-23 19:15:40 +0400

    feat: Set Dashboard as default area controller; add DashboardController
    - Changed default area controller to Dashboard in Program.cs
    - Removed Areas\Admin\Controllers\ from .csproj includes
    - Added DashboardController with Index action in Admin area
    - Added empty AppDbContext class in WebApplication4.Data
    - Updated .csproj.user to track empty MVC controller scaffolder selection

---
+++ WebApplication4/Data/AppDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication4.Data
+{
+    public class AppDbContext
+    {
+    }
+}
--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",

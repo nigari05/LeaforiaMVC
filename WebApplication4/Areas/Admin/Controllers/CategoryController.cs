85373b9aadb735908acf22ec68119c03c2289c31
Nigari Zulfuqarova <nigarizulfuqarova@gmail.com>
2026-08-25 15:21:21 +0400

    feat: Refactor CategoryController; add Index and Create views
    CategoryController now uses DI for AppDbContext and async Index action to fetch categories with books, ordered by name. Added Index.cshtml to list categories and Creaate.cshtml for category creation, both with improved UI and validation.

--- WebApplication4/Areas/Admin/Controllers/CategoryController.cs
+++ WebApplication4/Areas/Admin/Controllers/CategoryController.cs
@@ -1,10 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Data;
 
 namespace WebApplication4.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories
+                .Include(c => c.Books)
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+
+            return View(categories);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
